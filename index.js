df612e4586d9e0302f977a108957ea443aefc035
nouvellefemme <>
2021-08-21 07:52:59 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express     = require('express');
+const cors        = require('cors');
+const mongoose = require('mongoose');
+
+
+require('dotenv').config();
+//this one is dublicated in the new express version 
+//const bodyParser  = require('body-parser');
+
+
+
+
+
+let app = express();
+const port= process.env.PORT || 5000;
+app.use(cors());
+//so now no need to use this and use the one below instead 
+//app.use(bodyParser.json());
+app.use(express.json());
+
+//connect to your database using the following syntax
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true  }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
